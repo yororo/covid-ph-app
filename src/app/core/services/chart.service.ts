e0e5e5f0d27551f787f68b46fb049796046ba7a7
Sean Yoro <seanalvinyoro@gmail.com>
2020-04-19 21:59:47 +0800

    chart pie grid color scheme changes

--- src/app/core/services/chart.service.ts
+++ src/app/core/services/chart.service.ts
@@ -195,7 +195,7 @@ export class ChartService {
             legend: false,
             showLabels: true,
             colorScheme: {
-                domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+                domain: ['#afafaf', '#ab000c', '#d50006', '#ff0000', '#ff5555', '#ffa2a2']
             }
         }
         return options;
